917b34411055a7ccb87a5ca32b68b39371e456bd
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2025-03-10 22:41:06 -0600

    create basic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import MultiSelect from './component/MultiSelect'
+
+const options = [
+  "Line chart",
+  "Area chart",
+  "Column chart",
+  "Bar chart",
+  "Pie chart",
+  "Scatter chart",
+  "Bubble chart",
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="p-4 bg-[#E6E8E9] h-screen w-full grid place-items-center">
+      <MultiSelect
+        label="Chart type"
+        options={options}
+        value={selectedOptions}
+        onSelect={setSelectedOptions}
+      />
+    </div>
   )
 }
 
---
+++ src/component/MultiSelect/index.tsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react';
+
+export interface SidiSelectProps {
+    label: string;
+    options: string[];
+    id?: string;
+    value?: string[];
+    onSelect?: (options: string[]) => void;
+}
+
+const MultiSelect = ({
+    label = '',
+    options,
+    value = [],
+    onSelect,
+}: SidiSelectProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOptions, setSelectedOptions] = useState<string[]>(value);
+    const [search, setSearch] = useState('');
+
+    const handleOptionClick = (option: string) => {
+        setSelectedOptions((prevOptions) => {
+            const isSelected = prevOptions.includes(option);
+            const updatedOptions = isSelected
+                ? prevOptions.filter((selected) => selected !== option)
+                : [...prevOptions, option];
+
+            if (onSelect) {
+                onSelect(updatedOptions);
+            }
+            return updatedOptions;
+        });
+    };
+
+    const handleRemoveOption = (option: string) => {
+        setSelectedOptions((prevOptions) => {
+            const updatedOptions = prevOptions.filter((selected) => selected !== option);
+            if (onSelect) {
+                onSelect(updatedOptions);
+            }
+            return updatedOptions;
+        });
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter' && search.trim() !== '') {
+            handleOptionClick(search.trim());
+            setSearch('');
+        }
+    };
+
+    useEffect(() => {
+        if (JSON.stringify(selectedOptions) !== JSON.stringify(value)) {
+            setSelectedOptions(value);
+        }
+    }, [value, selectedOptions]);
+
+    const filteredOptions = options.filter(option => option.toLowerCase().includes(search.toLowerCase()));
+    const showNewValue = search.trim() !== '' && !options.includes(search.trim());
+
+    useEffect(() => {
+        setIsOpen(search.trim() !== '' && (filteredOptions.length > 0 || showNewValue));
+    }, [search, filteredOptions, showNewValue]);
+
+    return (
+        <div className='bg-red w-full max-w-[350px]'>
+            <label className='text-sm' htmlFor={label}>{label}</label>
+            <div className="relative min-h-3 cursor-pointer p-1 flex border border-gray-300 bg-white rounded-md">
+                <div className="flex flex-wrap gap-1 max-h-24 overflow-y-auto">
+                    {selectedOptions.map((option) => (
+                        <div key={option} className="flex items-center bg-[#E0E6E9] text-[#4A4F53] px-2 py-1 rounded-md text-xs">
+                            {option}
+                            <button className="ml-2 cursor-pointer text-[#7D818D] rounded-full" onClick={() => handleRemoveOption(option)}>×</button>
+                        </div>
+                    ))}
+                </div>
+
+                <input
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    className="p-2 text-sm border-transparent focus:outline-none w-full"
+                />
+
+                {isOpen && (
+                    <div className="absolute top-full left-0 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10 focus:border-[#0F70A9]">
+                        <ul className="max-h-32 overflow-y-auto">
+                            {filteredOptions.length > 0 ? (
+                                filteredOptions.map((option) => (
+                                    <li
+                                        key={option}
+                                        onClick={() => handleOptionClick(option)}
+                                        className="p-2 text-sm hover:bg-gray-100 cursor-pointer"
+                                    >
+                                        {option}
+                                    </li>
+                                ))
+                            ) : showNewValue ? (
+                                <li
+                                    className="p-2 text-sm hover:bg-gray-100 cursor-pointer flex justify-between"
+                                    onClick={() => {
+                                        handleOptionClick(search.trim());
+                                        setSearch('');
+                                    }}
+                                >
+                                    {search.trim()} <span className="text-gray-500">(new value)</span>
+                                </li>
+                            ) : (
+                                <li className="p-2 text-sm text-gray-500">No hay opciones disponibles</li>
+                            )}
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </div>
+
+    );
+};
+
+export default MultiSelect;
